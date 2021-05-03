da024d80165ddec4ff6ef9de01780bd0d499764b
Michael Reynolds <michael.reynolds@twosixlabs.com>
2021-05-03 15:49:23 -0400

    debugging

--- sofia/main.py
+++ sofia/main.py
@@ -185,8 +185,8 @@ class SOFIA:
             output_file = open(f'sofia/data/{experiment}_output/{file_name}.json', 'w')
             json.dump(data, output_file)
             return output_file
-        except:
-            print("Issue with file....")
+        except Exception as e:
+            print(e)
             return None
 
 
