c0cfc12e615b86fa23e2c5998a9f3f5f335874d0
Michael Reynolds <reynoldsm88@gmail.com>
2022-04-08 15:06:02 -0400

    [DART-1378] - workarounds and documentation about the processing for the march embed (#1)

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,7 @@ RUN yum upgrade -y
 
 RUN yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
     yum install -y net-tools which python3 enchant java-1.8.0-openjdk && \
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
-    python3 get-pip.py && \
-    rm get-pip.py && \
+    python3 -m ensurepip --default-pip && \
     yum clean all && \
     rm -r -f /var/cache/yum && \
     mkdir -p /opt/app/{bin,conf,data,pkg,tmp}
@@ -23,11 +21,15 @@ RUN yum install -y unzip && \
     rm /opt/app/pkg/*.zip && \
     chmod -R u+x $CORENLP_HOME
 
-COPY . /opt/app/bin
 
-RUN chmod -R u+x /opt/app/bin && \
-    cd /opt/app/bin && \
-    pip3 install -r requirements.txt && \
+COPY requirements.txt /opt/app/tmp/
+RUN pip3 install -r /opt/app/tmp/requirements.txt && \
     python3 -m nltk.downloader punkt
+
+COPY . /opt/app/bin
+RUN chmod -R u+x /opt/app/bin && \
+    cd /opt/app/bin
+
+COPY ontologies/* /opt/app/conf/
 
 ENTRYPOINT /opt/app/bin/sofia-stream.py worker
--- sofia/ontology_mapping.py
+++ sofia/ontology_mapping.py
@@ -1,9 +1,12 @@
 import json
-import string
 import os
-import nltk
-from nltk.corpus import stopwords
+import string
+from os.path import isabs
+from pathlib import Path
+
 import yaml
+from nltk.corpus import stopwords
+
 
 class Ontology:
 
@@ -23,89 +26,102 @@ class Ontology:
     #     self.indicators_WorldBank= self.get_indicators(indicator_path)
 
     def __init__(self, ontology_name):
-        self.dir= os.getcwd()
-        file_name = f'/data/Ontology_{ontology_name}.json'
-        #file_name = '/data/Ontology_wm.json'
-        self.external_ontology=False
+        self.dir = os.getcwd()
+
+        formatted_ontology_file = None
         if ontology_name == 'causex':
             self.external_ontology = True
-            file_name = './data/CauseX_Ontology.json'
-        file = os.path.dirname(os.path.abspath(__file__)) + file_name
-        if not os.path.exists(file):
-            self.format_ontology(ontology_name, file)
-        with open(file) as f:
-            text = f.read()
-            self.ontology= json.loads(text)
-        indicator_path = os.path.dirname(os.path.abspath(__file__)) + '/data/Indicators_WorldBank_Full.txt'
-        self.indicators_WorldBank= self.get_indicators(indicator_path)
+            formatted_ontology_file = './data/CauseX_Ontology.json'
+        if isabs(ontology_name):
+            self.external_ontology = True
+            formatted_ontology_file = f'/opt/app/tmp/{Path(ontology_name).with_suffix(".json").name}'
+        else:
+            formatted_ontology_file = os.path.dirname(
+                os.path.abspath(__file__)) + f'/data/Ontology_{ontology_name}.json'
+            self.external_ontology = False
 
+        print(f'using ontology {formatted_ontology_file}')
+        if not os.path.exists(formatted_ontology_file):
+            print('formatting ontology...')
+            self.format_ontology(ontology_name, formatted_ontology_file)
+            print('ontology formatted...')
+        with open(formatted_ontology_file) as f:
+            text = f.read()
+            self.ontology = json.loads(text)
+        indicator_path = os.path.dirname(os.path.abspath(__file__)) + '/data/Indicators_WorldBank_Full.txt'
+        self.indicators_WorldBank = self.get_indicators(indicator_path)
 
     def get_indicators(self, path):
-        f= open(path)
-        text= f.read().split('\n\n')
+        f = open(path)
+        text = f.read().split('\n\n')
         f.close()
-        indicators={}
+        indicators = {}
         for type in text:
-            terms= type.split('\n')
-            indicators[terms[0]]= terms[1:]
+            terms = type.split('\n')
+            indicators[terms[0]] = terms[1:]
         return indicators
 
     def string_matching(self, phrase, resource):
-        term_scores={}
+        term_scores = {}
         for key in self.indicators_WorldBank:
-            terms= self.indicators_WorldBank[key]
+            terms = self.indicators_WorldBank[key]
             for term in terms:
-                score= self.score(phrase, term)
-                if score>0.1:
-                    term_scores[term]= score
+                score = self.score(phrase, term)
+                if score > 0.1:
+                    term_scores[term] = score
         return term_scores
 
     def score(self, phrase, key_term):
-        punc= string.punctuation
-        stop_words= stopwords.words('english')
-        phrase= str(phrase.strip(' '))
+        punc = string.punctuation
+        stop_words = stopwords.words('english')
+        phrase = str(phrase.strip(' '))
         if phrase == key_term:
             return 1.0
         elif phrase in key_term:
-            return len(phrase.split(' '))/ len(key_term.split(' '))
+            return len(phrase.split(' ')) / len(key_term.split(' '))
         else:
-            score=0.0
-            phrase_words= []
+            score = 0.0
+            phrase_words = []
             for i in phrase.split(' '):
                 if i not in punc and i not in stop_words:
                     phrase_words.append(i)
             for word in phrase_words:
                 if word in key_term.split(' '):
-                    score+=1
-            return 2*score / (len(key_term.split(' '))+ len(phrase_words))
-
+                    score += 1
+            return 2 * score / (len(key_term.split(' ')) + len(phrase_words))
 
     def refine_word(self, sentence, lemma, pos):
-        #if self.externalOntology: return self.refineWord_external(sentence, lemma, pos, fnFrames)
-        events1, events2, entities= self.ontology['event'], self.ontology['property'], self.ontology['entity']
+        # if self.externalOntology: return self.refineWord_external(sentence, lemma, pos, fnFrames)
+        events1, events2, entities = self.ontology['event'], self.ontology['property'], self.ontology['entity']
         for type in events1.keys():
             if lemma in events1[type]:
-                return 'event/'+type, 'event'
+                return 'event/' + type, 'event'
         for type in events2.keys():
             if lemma in events2[type]:
-                return 'property/'+type, 'property'
+                return 'property/' + type, 'property'
         for type in entities.keys():
             if lemma in entities[type]:
                 return type, 'entity'
         return "", ""
 
     def format_ontology(self, ontology_name, save_file):
-        file_name = f'/data/{ontology_name}.yml'
-        #file_path = os.path.dirname(os.path.abspath(file_name))+file_name
-        file = os.path.dirname(os.path.abspath(__file__)) + file_name
+        print(f'formatting ontology {ontology_name} to {save_file}')
+        if isabs(ontology_name):
+            file = ontology_name
+        else:
+            file_name = f'/data/{ontology_name}.yml'
+            file = os.path.dirname(os.path.abspath(__file__)) + file_name
+
         with open(file) as f:
-            data= yaml.full_load(f)
+            data = yaml.full_load(f)
         data = data[0]['wm']
-        path= ''
+        path = ''
         output = {'entity': {}, 'event': {}, 'property': {}}
         output = recurse(data, path, output)
         with open(save_file, 'w') as f:
             json.dump(output, f)
+
+        print(f'finished formatting ontology {ontology_name} to {save_file}')
         return output
 
 
@@ -116,12 +132,13 @@ def recurse(data, path, output):
             path_new = f"{path}/{data[i]['name']}"
             path_new = path_new.strip('/')
             output[semantic_type][path_new] = data[i]['examples']
-            #return output
+            # return output
         else:
             k = list(data[i].keys())[0]
             data_new = data[i][k]
             path_new = f"{path}/{k}"
             path_new = path_new.strip('/')
-            output_new= recurse(data_new, path_new, output)
+            output_new = recurse(data_new, path_new, output)
             for j in output_new: output[j].update(output_new[j])
     return output
+
